e4e5f3a3416b33a928cd5a56e6c5136c0e24f972
lincmin <lincm69696@qq.com>
2017-11-18 20:51:54 +0800

    标签导航tab添加Weather菜单

--- src/routers/TabNav.js
+++ src/routers/TabNav.js
@@ -1,6 +1,7 @@
 import { TabNavigator, TabBarBottom } from 'react-navigation';
 import HomeScreen from '../pages/HomePage';
 import AboutScreen from '../pages/AboutPage';
+import WeatherScreen from '../pages/WeatherPage';
 
 const TabNav = TabNavigator(
     {
@@ -15,6 +16,13 @@ const TabNav = TabNavigator(
             screen: AboutScreen,
             navigationOptions: ({ navigation }) => ({
                 tabBarLabel: '关于',
+            }),
+        },
+
+        Weather: {
+            screen: WeatherScreen,
+            navigationOptions: ({ navigation }) => ({
+                tabBarLabel: '天气',
             }),
         },
     },
